709d5cbec0d53fcf01d02f38787682bde85f1db9
troido <troido@protonmail.com>
2021-08-03 23:13:05 +0200

    separated model from view and input for notes

---
+++ whistletab/renderer.js
@@ -0,0 +1,94 @@
+
+
+class Renderer {
+    
+    constructor(key, spacing){
+        this.key = key;
+        this.spacing = spacing;
+    }
+    
+    render(line){
+        if (line instanceof Line.Empty){
+            return [];
+        } else if (line instanceof Line.Comment) {
+            return [this.renderComment(line)];
+        } else if (line instanceof Line.Lyrics) {
+            return [this.renderLyrics(line)];
+        } else if (line instanceof Line.Notes) {
+            return [this.renderTab(line)]; // todo: render staff
+        } else {
+            throw new Exception("unknown line type");
+        }
+    }
+    
+    renderComment(line){
+        let para = document.createElement('p');
+        para.appendChild(document.createTextNode(line.text));
+        para.className = line.isHeading ? 'comment heading' : 'comment text';
+        return para;
+    }
+    
+    renderLyrics(line){
+        let para = document.createElement('p');
+        line.words.forEach(word => {
+            let spacingWrapper = document.createElement('span');
+            let wordEl = document.createElement('span');
+            wordEl.appendChild(document.createTextNode(word));
+            wordEl.className = 'word';
+            spacingWrapper.className = 'spacer';
+            spacingWrapper.appendChild(wordEl);
+            para.appendChild(spacingWrapper);
+        });
+        para.className = 'comment lyric';
+        return para;
+    }
+    
+    renderTab(line){
+        let el = document.createElement("div");
+        for (let note of line.notes){
+            el.appendChild(this.renderTabPart(note));
+        }
+        return el;
+    }
+    
+    renderTabPart(part){
+        if (part instanceof Token.AbsoluteNote){
+            return this.renderNote(part);
+        } else if (part instanceof Token.Rest) {
+            return document.getElementById("tab-entry-spacer").content.cloneNode(true);
+        } else if (part instanceof Token.Slur) {
+            return document.getElementById("tab-entry-slur").content.cloneNode(true);
+        }
+    }
+    
+    renderNote(note){
+        let tabMissing = false;
+        let relative = note.makeRelative(this.key)
+        let fingering = relative
+            .getFingering();
+        let tab = document.getElementById("tab-entry").content.cloneNode(true);
+        let symbols;
+        if (fingering === null) {
+            tab.querySelector("ul").classList.add("error");
+            symbols = "??????".split('');
+        } else {
+            symbols = fingering
+                .split('')
+                .map(finger => SYMBOL_MAP[finger]);
+        }
+
+        let items = tab.querySelectorAll("li")
+        symbols.forEach((symbol, index) => {
+            items[index].textContent = symbol;
+        });
+        let name = NOTES[note.code % 12];
+        if (relative.getOctave() >= 1){
+            name = name.toUpperCase();
+        }
+        name = name.replace("#", '<span class="sharp">\u266f</span>');
+        tab.querySelector(".tab-note--letter").innerHTML = name;
+        tab.querySelector(".note-octave").textContent = "+".repeat(relative.getOctave());
+        
+        return tab
+    }
+}
--- whistletab/tabview.js
+++ whistletab/tabview.js
@@ -29,111 +29,6 @@ const SYMBOL_MAP = {
 
 let NOTES = "a a# b c c# d d# e f f# g g#".split(' ');
 
-class WrittenNote {
-    
-    static noteParser = /^([a-gA-G])(#*|_*)(\+*)$/;
-    
-    
-    static fromString(name){
-        let match = name.match(this.noteParser);
-        if (!match || match.length !== 4){
-            return null;
-        }
-        let [_, letter, suffix, higher] = match;
-        let octave = higher.length;
-        if (letter.match(/[A-G]/)) {
-            ++octave;
-            letter = letter.toLowerCase();
-        }
-        return new WrittenNote(letter, octave, suffix);
-    }
-    
-    constructor(letter, octave, suffix){
-        this.letter = letter;
-        this.octave = octave;
-        this.suffix = suffix;
-    }
-    
-    getCode(base){
-        base = base || 0;
-        return (LETTER_CODES[this.letter] + 12 - base) % 12 + base
-            + 12 * this.octave 
-            + (this.suffix === SHARP) 
-            - (this.suffix === FLAT);
-    }
-    
-    makeAbsolute(baseCode){
-        return new Token.AbsoluteNote(
-            this.getCode(baseCode), 
-            (this.suffix === SHARP) - (this.suffix === FLAT)
-        );
-    }
-}
-
-class Token {
-
-    static AbsoluteNote = class AbsoluteNote{
-        
-        constructor(code, shift){
-            this.code = code;
-            this.shift = shift || 0; // whether the key is sharp of flat. This won't change the actual height, only whether it's displayed as eg C# or Db
-        }
-        
-        makeRelative(keyCode){
-            return new RelativeNote(this.code - keyCode);
-        }
-        
-        getOctave(baseCode){
-            return ((this.code - baseCode) / 12) | 0;
-        }
-        
-        getTab(keyCode){
-            
-            let tabMissing = false;
-            let relative = this.makeRelative(keyCode)
-            let fingering = relative
-                .getFingering();
-            let tab = document.getElementById("tab-entry").content.cloneNode(true);
-            let symbols;
-            if (fingering === null) {
-                tab.querySelector("ul").classList.add("error");
-                symbols = "??????".split('');
-            } else {
-                symbols = fingering
-                    .split('')
-                    .map(finger => SYMBOL_MAP[finger]);
-            }
-
-            let items = tab.querySelectorAll("li")
-            symbols.forEach((symbol, index) => {
-                items[index].textContent = symbol;
-            });
-            let name = NOTES[this.code % 12];
-            if (relative.getOctave() >= 1){
-                name = name.toUpperCase();
-            }
-            name = name.replace("#", '<span class="sharp">\u266f</span>');
-            tab.querySelector(".tab-note--letter").innerHTML = name;
-            tab.querySelector(".note-octave").textContent = "+".repeat(relative.getOctave());
-            
-            return tab
-        }
-        
-    }
-    
-    static Rest = class Rest{
-        getTab(keyCode){
-            return document.getElementById("tab-entry-spacer").content.cloneNode(true);
-        }
-    }
-    
-    static Slur = class Slur{
-        getTab(keyCode){
-            return document.getElementById("tab-entry-slur").content.cloneNode(true);
-        }
-    }
-}
-
 class RelativeNote {
     
     constructor(code){
@@ -190,130 +85,9 @@ class RelativeNote {
         'oooooo'
     ];
 }
-        
-
-class WTText {
-    
-    constructor(text, base){
-        this.text = text;
-        this.base = base;
-    }
-    
-    toLines(){
-        if (this.text === null || this.text === undefined){
-            return [];
-        }
-        return this.text
-            .split('\n')
-            .map(l => this.textLineToLine(l.trim()));
-    }
-    
-    textLineToLine(text){
-        if (text === ""){
-            return new Line.Empty();
-        } else {
-            let parts = text.match(/^(?<prefix>-*)(?<body>[^-].*)$/).groups
-            let prefix = parts.prefix;
-            let body = parts.body;
-            if (prefix.length > 3){
-                return new Line.Empty();
-            } else if (prefix.length === 3){
-                return new Line.Comment(body, true);
-            } else if (prefix.length === 2){
-                return new Line.Lyrics(body.split(' '));
-            } else if (prefix.length === 1){
-                return new Line.Comment(body);
-            } else if (prefix.length === 0){
-                return this.toNoteLine(body);
-            } else {
-                throw new TextError(`no match found for line prefix: ${prefix}, ${body}; line: ${text}`);
-            }
-        }
-    }
-    
-    toNoteLine(line){
-        let text = line;
-        let tokens = [];
-        while (text !== ""){
-            let r = text.match(/^(\s+)|(-)|([a-gA-G])(#*|_*)(\+*)/);
-            if (r === null){
-                throw new TextError(`invalid line: ${text}`);
-            }
-            let [match, space, slur, noteName, suffix, octave] = r;
-            if (space !== undefined){
-                tokens.push(new Token.Rest())
-            } else if (slur !== undefined){
-                tokens.push(new Token.Slur());
-            } else if (noteName != undefined){
-                tokens.push(WrittenNote.fromString(match).makeAbsolute(this.base));
-            }
-            text = text.substring(match.length);
-        }
-        return new Line.Notes(tokens);
-    }
-}
 
 
-class Line {
-    
-    static Empty = class Empty {
-        render(){
-            return [];
-        }
-    }
-    
-    static Comment = class Comment {
-        
-        constructor(text, isHeading){
-            this.text = text;
-            this.isHeading = isHeading || false;
-        }
-        
-        render() {
-            let para = document.createElement('p');
-            para.appendChild(document.createTextNode(this.text));
-            para.className = this.isHeading ? 'comment heading' : 'comment text';
-            return [para];
-        }
-    }
-    
-    static Lyrics = class Lyrics{
-        
-        constructor(words){
-            this.words = words
-        }
-        
-        render(){
-            let para = document.createElement('p');
-            this.words.forEach(word => {
-                let spacingWrapper = document.createElement('span');
-                let wordEl = document.createElement('span');
-                wordEl.appendChild(document.createTextNode(word));
-                wordEl.className = 'word';
-                spacingWrapper.className = 'spacer';
-                spacingWrapper.appendChild(wordEl);
-                para.appendChild(spacingWrapper);
-            });
-            para.className = 'comment lyric';
-            return [para];
-        }
-    }
-    
-    static Notes = class Notes {
-        
-        constructor(notes) {
-            this.notes = notes;
-        }
-        
-        render(key){
-            let el = document.createElement("div");
-            for (let note of this.notes){
-                el.appendChild(note.getTab(key));
-            }
-            return [el];
-        }
-    }
-}
+
 
 const SAME = "s";
 
@@ -323,62 +97,10 @@ class TabView {
     spacerTemplate = '<span class="spacer"></span>'
     slurTemplate = '<span class="slur">(</span>'
 
-    noteMatcher = /^-{1,3}.*$|-|[a-g]#?\+{0,2}|\n| /gi
-    
-    noteLineValidator = /^(\s+|-|[a-g](?:#*|_*)\+*)+$/i
-    noteTokenizer = /(?<space>\s+)|(?<slur>-)|(?<notename>[a-gA-G])(?<suffix>#*|_*)(?<octave>\+*)/g
 
     spacing = 1
     // These are the spacings as defined in ems in the CSS
     spacings = [0, 0.1, 0.2, 0.3, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8]
-    
-
-    fingerings = [
-        '------',
-        '-----h',
-        '-----o',
-        '----ho',
-        '----oo',
-        '---ooo',
-        '--Hooo',
-        '--oooo',
-        '-Hoooo',
-        '-ooooo',
-        'o--ooo',
-        'oooooo',
-
-        'o-----',
-        '-----h',
-        '-----o',
-        '----ho',
-        '----oo',
-        '---ooo',
-        '--o--o',
-        '--oooo',
-        '-o-ooo',
-        '-ooooo',
-        'o-o---',
-        'ooo---',
-
-        'o-----',
-        null,
-        '-----o',
-        null,
-        '----oo',
-        '---ooo',
-        null,
-        'o----o',
-        null,
-        '-ooooo',
-        null,
-        'oooooo'
-    ]
-    symbolMap = {
-        'o': '\u25cb', // white circle
-        '-': '\u25cf', // black circle
-        'H': '\u25d0', // circle with left half black
-        'h': '\u25d1'  // circle with right half black
-    }
     
     
     constructor() {
@@ -413,73 +135,11 @@ class TabView {
         return this.emSize * (this.spacings[this.spacing] + 1.2);
     }
 
-    lyricsFromNote(note) {
-        var commentWords = note.replace(/^--(.*)$/, '$1').split(' ');
-        var para = document.createElement('p');
-
-        commentWords.forEach(function (word) {
-            var spacingWrapper = document.createElement('span');
-            var wordEl = document.createElement('span');
-            var textNode = document.createTextNode(word);
-            wordEl.appendChild(textNode);
-            wordEl.className = 'word';
-            spacingWrapper.className = 'spacer';
-            spacingWrapper.appendChild(wordEl);
-            para.appendChild(spacingWrapper);
-        });
-        para.className = 'comment lyric';
-
-        return para.outerHTML;
-    }
-    commentFromNote(note, isHeading) {
-        var para = document.createElement('p');
-        var text = note.replace(/^-{1,3}(.*)$/, '$1');
-        para.appendChild(document.createTextNode(text));
-        para.className = isHeading ? 'comment heading' : 'comment text';
-        return para.outerHTML;
-    }
-    noteTemplate(note) {
-        var htmlNote;
-        var octave = '';
-
-        if (/[a-g](#)?\+/.test(note)) {
-            note = note.toUpperCase().slice(0, -1);
-        }
-        // Uppercase notes are shorthand for `<note>+`
-        if (/[A-G]/.test(note)) {
-            octave = '+';
-        }
-        if (/\+/.test(note)) {
-            note = note.toUpperCase().slice(0, -1);
-            octave = '++';
-        }
-
-        htmlNote = '<span class="tab-note--letter">' + note + '</span>';
-        htmlNote = htmlNote.replace('#', '<span class="sharp">\u266f</span>'); // sharp symbol
-        if (octave) {
-            htmlNote += '<sup>' + octave + '</sup>';
-        }
-
-        return htmlNote;
-    }
     staffFromNotes(notes) {
         const noteSpacing = this.calculateNoteSpacing();
         const newStaff = new window.Staff(notes, this.showNotes, noteSpacing);
         this.staves.push(newStaff);
         return newStaff.toHtml();
-    }
-    
-    getFingering(noteName, baseCode, keyCode){
-        
-        let note = WrittenNote.fromString(noteName);
-        if (note === null){
-            return null;
-        }
-        let code = note.getCode(baseCode) - keyCode;
-        if (code >= this.fingerings.length){
-            return null;
-        }
-        return this.fingerings[code]
     }
     
     tabFromNote(note, staffNotes, baseCode, keyCode) {
@@ -548,9 +208,11 @@ class TabView {
 
         this.el.innerHTML = '';
         
-        let wt = new WTText(inputString, baseCode);
-        wt.toLines(baseCode)
-            .reduce(((acc, line) => acc.concat(line.render(keyCode))), [])
+        let renderer = new Renderer(keyCode, this.spacing);
+        
+        let wt = new WTText(baseCode);
+        let lines = wt.toLines(inputString)
+            .reduce(((acc, line) => acc.concat(renderer.render(line))), [])
             .forEach((line, index) => {
                 if (index != 0){
                     this.el.appendChild(document.getElementById("line-break-template").content.cloneNode(true));
@@ -570,23 +232,6 @@ class TabView {
     setSpacing(toValue) {
         this.spacing = parseInt(toValue, 10);
         this.el.className = 'spacing' + this.spacing;
-
-        if (this.staves.length === 0) return;
-
-        const newStaves = [];
-        const staffEls = Array.from(this.el.querySelectorAll('.staff'));
-
-        staffEls.forEach(function (el, index) {
-            const oldStaff = this.staves[index];
-            const notes = oldStaff.notes;
-            const noteSpacing = this.calculateNoteSpacing();
-            const newStaff = new window.Staff(notes, this.showNotes, noteSpacing);
-            newStaff.render();
-            this.el.replaceChild(newStaff.svg, el);
-            newStaves.push(newStaff);
-        }, this);
-
-        this.staves = newStaves;
     }
     
     setName(name){
